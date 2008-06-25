6f07ccdc8f5eb069d1a90bcad1f77740fdb81db9
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-06-25 09:51:55 +0000

    deep-connect version:   ffile   here
    git-svn-id: http://localhost/svn/repos/fairy@72 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.0-002-ffile+here/backend/b-inputtable.rb
@@ -0,0 +1,35 @@
+
+
+module Fairy
+  module BInputtable
+
+    def input=(input)
+      super
+      start_create_nodes
+    end
+
+    def start_create_nodes
+      Thread.start do
+	create_nodes
+      end
+    end
+
+    def create_nodes
+      no = 0
+      @input.each_export do |export|
+	create_and_add_node(export)
+	no += 1
+      end
+      self.number_of_nodes = no
+    end
+
+    def create_and_add_node(export)
+      node = create_node
+      node.input= export
+      export.output = node.import
+      add_node node
+      node
+    end
+
+  end
+end
---
+++ tags/v0.2.0-002-ffile+here/backend/controller.rb
@@ -0,0 +1,39 @@
+
+require "deep-connect/deep-connect.rb"
+
+require "backend/job-interpriter"
+require "backend/scheduler"
+
+module Fairy
+
+  class Controller
+    
+    def initialize
+      @job_interpriter = JobInterpriter.new(self)
+      @scheduler = Scheduler.new(self)
+    end
+
+    def send_atom(atom)
+      @job_interpriter.exec(atom)
+    end
+
+    #
+    # BEGIN DFRQ
+    # * サービスの立ち上げ
+    #
+    def start(service)
+      @deepconnect = DeepConnect.start(service)
+      @deepconnect.register_service("Controller", self)
+      @deepconnect.register_service("BFile", BFile)
+      @deepconnect.register_service("BHere", BHere)
+    end
+
+    def Controller.start(service)
+      controller = Controller.new
+      controller.start(service)
+    end
+    #
+    # END DFRQ
+    #
+  end
+end
