34bb4d958786b817235cd7cd5c46dd4e4e9c41f6
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-07-01 13:50:36 +0000

    deep-connect v0.2.02-003対応版
    git-svn-id: http://localhost/svn/repos/fairy@80 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.1-003-deep-connect-v0.2.02-003/node/n-each-substream-mapper.rb
@@ -0,0 +1,22 @@
+
+require "node/njob"
+require "node/n-single-exportable"
+
+module Fairy
+  class NEachSubStreamMapper<NFilter
+    include NSingleExportable
+
+    def initialize(bjob, block_source)
+      super(bjob)
+      @block_source = block_source
+      @map_proc = eval("proc{#{@block_source}}", TOPLEVEL_BINDING)
+    end
+
+    def start
+      super do
+	@map_proc.call(@import, @export)
+      end
+    end
+  end
+
+end
