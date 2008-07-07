a028168a7392b9553dbc03ae220b1d4cf676285b
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-07-07 10:35:07 +0000

    * zip(完全分散版)
    git-svn-id: http://localhost/svn/repos/fairy@101 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.2-005-zip/backend/bfile.rb
@@ -0,0 +1,37 @@
+require "uri"
+
+require "backend/binput"
+
+module Fairy
+  class BFile<BInput
+    def BFile.open(controller, descripter)
+      descripter = descripter.to_a
+      bfile = BFile.new(controller)
+      bfile.open(descripter)
+      bfile
+    end
+
+    URI_REGEXP = /:\/\//
+
+    def open(descripter)
+      no = 0
+      for file in descripter
+	no +=1
+
+	host = "localhost"
+	path = file
+	if URI_REGEXP =~ file
+	  uri = URI(file)
+	  host = uri.host
+	  path = uri.path
+	end
+	processor = @controller.assign_input_processor(host)
+	node = processor.nfile_open(self, path)
+# 	node_klass = processor.NFile
+# 	node = node_klass.open(self, uri.path)
+	add_node node
+      end
+      self.number_of_nodes = no
+    end
+  end
+end
