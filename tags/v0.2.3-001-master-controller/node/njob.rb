63f101b5539d0c6a736bd5acd2ff0cac2894cb39
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-07-11 08:15:11 +0000

    * processorを割り当てるのをmasterからcontroller?へ  * Controller#assign_new_processor_n 追加
    git-svn-id: http://localhost/svn/repos/fairy@103 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.3-001-master-controller/node/njob.rb
@@ -0,0 +1,82 @@
+
+require "thread"
+
+module Fairy
+
+  class NJob
+
+    END_OF_STREAM = :END_OF_STREAM
+
+    ST_INIT = :ST_INIT
+    ST_ACTIVATE = :ST_ACTIVATE
+    ST_FINISH = :ST_FINISH
+
+    def initialize(processor, bjob)
+      puts "CREATE NJOB: #{self.class}"
+      @processor = processor
+      @bjob = bjob
+
+      @status = ST_INIT
+      @status_mutex = Mutex.new
+      @status_cv = ConditionVariable.new
+
+      start_watch_status
+    end
+
+    attr_reader :processor
+
+    def start(&block)
+      puts "START NJOB: #{self.class}"
+      Thread.start do
+	self.status = ST_ACTIVATE
+	begin
+	  block.call
+	ensure
+	  self.status = ST_FINISH
+	end
+      end
+    end
+
+    def status=(val)
+      @status_mutex.synchronize do
+	@status = val
+	@status_cv.broadcast
+      end
+    end
+
+    def start_watch_status
+      Thread.start do
+	old_status = nil
+	loop do
+	  @status_mutex.synchronize do
+	    while old_status == @status
+	      @status_cv.wait(@status_mutex)
+	    end
+	    old_status = @status
+	  end
+	  notice_status(@status)
+	end
+      end
+    end
+
+    def start_watch_status0
+      Thread.start do
+	old_status = nil
+	@status_mutex.synchronize do
+	  loop do
+	    while old_status == @status
+	      @status_cv.wait(@status_mutex)
+	    end
+	    old_status = @status
+	    notice_status(@status)
+	  end
+	end
+      end
+    end
+
+    def notice_status(st)
+      @bjob.update_status(self, st)
+    end
+
+  end
+end
---
+++ tags/v0.2.3-001-master-controller/share/debug.rb
@@ -0,0 +1,17 @@
+
+module Fairy
+  module Debug
+    def njob_status_monitor_on(fairy)
+#      require "backend/bjob"
+      
+      bjob = fairy.name2backend_class("BJob")
+      bjob.watch_status = true
+    end
+
+    for method in self.instance_methods
+      module_function method
+    end
+
+  end
+end
+
