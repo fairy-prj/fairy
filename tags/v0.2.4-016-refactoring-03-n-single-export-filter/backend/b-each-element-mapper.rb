9311ec3d4bbd3f10edd41865c593589deba32e55
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-08-15 06:16:22 +0000

    * refactoring 03    - NSingleExportFilter を追加
    git-svn-id: http://localhost/svn/repos/fairy@185 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.4-016-refactoring-03-n-single-export-filter/backend/b-each-element-mapper.rb
@@ -0,0 +1,22 @@
+
+require "backend/b-filter"
+require "backend/b-inputtable"
+
+module Fairy
+  class BEachElementMapper<BFilter
+    Controller.def_export self
+
+    def initialize(controller, opts, block_source)
+      super
+      @block_source = block_source
+    end
+
+    def node_class_name
+      "NEachElementMapper"
+    end
+
+    def njob_creation_params
+      [@block_source]
+    end
+  end
+end
---
+++ tags/v0.2.4-016-refactoring-03-n-single-export-filter/version.rb
@@ -0,0 +1,8 @@
+#
+# Don't modified this file.
+# This file is auto generation. 
+#
+module Fairy
+    Version = "0.2.4-016"
+end
+
