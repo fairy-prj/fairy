9bfe80d54fce444d5f0405c953299d6603112812
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-08-18 10:10:15 +0000

    * varray: バグ解消
    git-svn-id: http://localhost/svn/repos/fairy@204 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.5-005-varray/share/varray.rb
@@ -0,0 +1,69 @@
+
+module Fairy
+  class VArray
+    include Enumerable
+
+    def self.output(fairy, opts)
+      OutputVArray.output(fairy, opts)
+    end
+
+    def initialize
+      @arrays = nil
+      @arrays_mutex = Mutex.new
+      @arrays_cv = ConditionVariable.new
+    end
+
+    def arrays
+      @arrays_mutex.synchronize do
+	while @arrays.nil?
+	  @arrays_cv.wait(@arrays_mutex)
+	end
+	@arrays
+      end
+    end
+
+    def set_arrays(array)
+      @arrays = array
+      @arrays_cv.broadcast
+    end
+
+    def arrays_size
+      self.arrays.size
+    end
+
+    def arrays_put(idx, array)
+      @arrays_mutex.synchronize do
+	while @arrays.nil?
+	  @arrays_cv.wait(@arrays_mutex)
+	end
+	@arrays[idx] = array
+	@arrays_cv.broadcast
+      end
+    end
+
+    def arrays_at(idx)
+      @arrays_mutex.synchronize do
+	while @arrays.nil? or @arrays[idx].nil?
+	  @arrays_cv.wait(@arrays_mutex)
+	end
+	@arrays[idx]
+      end
+    end
+
+    def each(&block)
+      
+      # set_arrayされるまでまっている.
+      arrays.size.times do |idx|
+	ary = nil
+	@arrays_mutex.synchronize do
+	  while @arrays[idx].nil?
+	    @arrays_cv.wait(@arrays_mutex)
+	  end
+	  ary = @arrays[idx]
+	end
+	ary.each(&block)
+      end
+    end
+  end
+end
+
