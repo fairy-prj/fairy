eefde4051c843302dc3046d88f90df3680361fc0
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-08-18 10:38:10 +0000

    * VArray#[], []=
    git-svn-id: http://localhost/svn/repos/fairy@207 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.5-006-varray-01-at-atput/node/n-input-iota.rb
@@ -0,0 +1,22 @@
+
+module Fairy
+  class NIota<NSingleExportInput
+    Processor.def_export self
+
+    def initialize(processor, bjob, opts, first, last)
+      super
+      @first = first
+      @last = last
+    end
+
+    def start
+      super do
+	for i in @first..@last
+	  @export.push i
+	end
+      end
+    end
+
+  end
+end
+
