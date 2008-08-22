a7503cf54f4647bc28b1b58ad86ae3a0e4d19d2c
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-08-22 08:05:26 +0000

    * k-means 02  * deep connect: reference 強化版に対応
    git-svn-id: http://localhost/svn/repos/fairy@216 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.5-009-k-means-02/fairy.rb
@@ -0,0 +1,58 @@
+
+require "deep-connect/deep-connect.rb"
+
+#DeepConnect::Organizer.immutable_classes.push Array
+
+
+module Fairy
+
+  class Fairy
+
+    def initialize(master_host, master_port)
+      @name2backend_class = {}
+
+      @deep_connect = DeepConnect.start(0)
+      @master_deepspace = @deep_connect.open_deepspace(master_host, master_port)
+      @master = @master_deepspace.import("Master")
+
+      @controller = @master.assgin_controller
+    end
+
+    attr_reader :controller
+
+    def name2backend_class(backend_class_name)
+      if klass = @name2backend_class[backend_class_name]
+	return klass 
+      end
+      
+      if klass =  @controller.import(backend_class_name)
+	@name2backend_class[backend_class_name] = klass
+      end
+      klass
+    end
+
+    def def_pool_variable(vname, value = nil)
+      @controller.def_pool_variable(vname, value)
+    end
+
+    def pool_variable(vname, *value)
+      @controller.pool_variable(vname, *value)
+    end
+
+    def self.def_fairy_interface(mod)
+      include mod
+    end
+  end
+
+  def def_fairy_interface(mod)
+    ::Fairy::Fairy.instance_eval{include mod}
+  end
+  module_function :def_fairy_interface
+
+end
+
+require "job/job"
+require "job/input"
+
+require "job/addins"
+
---
+++ tags/v0.2.5-009-k-means-02/node.rb
@@ -0,0 +1,61 @@
+
+require "deep-connect/deep-connect.rb"
+#DeepConnect::Organizer.immutable_classes.push Array
+
+module Fairy
+  class Node
+
+    PROCESSOR_BIN = "bin/processor"
+
+    def initialize
+      @addr = nil
+
+      @processors = []
+      @processors_mutex = Mutex.new
+      @processors_cv = ConditionVariable.new
+    end
+
+    attr_accessor :addr
+    attr_reader :processors
+
+    def start(master_host, master_port, service=0)
+      @deepconnect = DeepConnect.start(service)
+      @deepconnect.export("Node", self)
+
+      @master_deepspace = @deepconnect.open_deepspace(master_host, master_port)
+      @master = @master_deepspace.import("Master")
+      @master.register_node(self)
+    end
+
+    def create_processor
+      @processors_mutex.synchronize do
+	processor_id = @processors.size
+#	Process.spawn("test/testn.rb", 
+#		      "--controller", @deepconnect.local_id, 
+#		      "--id", processor_id.to_s)
+	Process.fork do
+	  exec(PROCESSOR_BIN,
+	       "--node", @deepconnect.local_id.to_s, 
+	       "--id", processor_id.to_s)
+	end
+	while !@processors[processor_id]
+	  @processors_cv.wait(@processors_mutex)
+	end
+	@processors[processor_id]
+      end
+    end
+      
+    def register_processor(processor)
+      @processors_mutex.synchronize do
+	@processors[processor.id] = processor
+	processor.addr = @addr
+	@processors_cv.broadcast
+      end
+    end
+
+    def Node.start(master_host, master_port)
+      node = Node.new
+      node.start(master_host, master_port)
+    end
+  end
+end
