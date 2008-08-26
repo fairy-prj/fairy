88ce7cd1f10a2076167d9a59a570dc8a76f33972
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-08-26 06:08:19 +0000

    * ruby1.9対応完了
    git-svn-id: http://localhost/svn/repos/fairy@223 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.5-011-ruby19-02/backend/b-inputtable.rb
@@ -0,0 +1,46 @@
+
+
+module Fairy
+  module BInputtable
+
+    def initialize(*rests)
+      super
+      @input = nil
+    end
+
+    def input=(input)
+      @input = input
+#      input.output = @input
+
+      start_create_nodes
+    end
+
+    attr_reader :input
+
+    def start_create_nodes
+      puts "START_CREATE_NODES: #{self}"
+      Thread.start do
+	create_nodes
+      end
+    end
+
+    def create_nodes
+      no = 0
+      @input.each_export do |export, node|
+	create_and_add_node(export, node)
+	no += 1
+      end
+puts "CREATE_NODES: #{self}.number_of_nodes=#{no}"
+      self.number_of_nodes = no
+    end
+
+    def create_and_add_node(input_export, input_node)
+      processor = @controller.assign_inputtable_processor(self, @input, input_node, input_export)
+      node = create_node(processor)
+      node.input= input_export
+      input_export.output = node.import
+      node
+    end
+
+  end
+end
