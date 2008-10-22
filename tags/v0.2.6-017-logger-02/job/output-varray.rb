642b4e88d17ce6c0d01ccd894ad72ee306e34995
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-10-22 10:56:23 +0000

    * node.rb/controller.rb/processor.rb: Log初期化の位置がまずかった  * Log::log: @loggerが設定されていなくても例外を出さなくした.             年とマイクロ秒を出すようにした.  * Log::debug_p: 追加  * Log::log_exception: API変更, $!をデフォルトパラメータにした
    git-svn-id: http://localhost/svn/repos/fairy@294 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.6-017-logger-02/job/output-varray.rb
@@ -0,0 +1,46 @@
+
+require "job/job"
+require "share/varray"
+
+module Fairy
+  class OutputVArray<Job
+    module Interface
+      # Usage:
+      # ... .to_va
+      #
+      def to_va
+	output_va = OutputVArray.output(@fairy, opts=nil)
+	output_va.input = self
+	output_va.varray
+      end
+    end
+    Fairy::def_job_interface Interface
+    
+    def self.output(fairy, opts)
+      output = new(fairy, opts)
+      output
+    end
+
+    def initialize(fairy, opts=nil)
+      super
+
+      @varray = backend.varray
+    end
+
+    attr_reader :varray
+
+    def backend_class_name
+      "BOutputVArray"
+    end
+
+    def output
+      backend.output
+    end
+
+    def input=(job)
+      @input = job
+      backend.input=job.backend
+#      backend.wait_all_output_finished
+    end
+  end
+end
---
+++ tags/v0.2.6-017-logger-02/job/output.rb
@@ -0,0 +1,24 @@
+module Fairy
+  
+  module OutputInterface
+    def output(vfn, opts = nil)
+      if vfn.kind_of?(Class)
+	outputter = vfn.output(@fairy, opts)
+      elsif !vfn.kind_of?(String) || VFile.vfile?(vfn)
+	outputter = FFileOutput.output(@fairy, opts, vfn)
+      else
+	outputter = LFileOutput.output(@fairy, opts, vfn)
+      end
+      outputter.input = self
+      outputter
+
+    end
+  end
+  def_job_interface OutputInterface
+
+end
+
+require "job/ffile-output"
+require "job/local-file-output"
+
+
