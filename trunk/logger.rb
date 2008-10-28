ae7f606e70dc4577cede8021770263d2b1f46537
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-10-28 08:49:18 +0000

    * Logger#start_flusher: 定期的に出力バッファをflushするようにした.
    git-svn-id: http://localhost/svn/repos/fairy@299 a864a6dc-dab1-42d4-9391-a69d331a00af

--- trunk/logger.rb
+++ trunk/logger.rb
@@ -5,13 +5,36 @@ module Fairy
 
     LOG_FILE = "/tmp/fairy/log"
 
+    FLUSH_INTERVAL = 10
+
     def initialize(path = LOG_FILE)
       @log_out = File.open(LOG_FILE, "a+")
+      
       @mutex = Mutex.new
+      @buffered = false
+
+      start_flusher
+    end
+
+    def start_flusher
+      Thread.start do
+	loop do
+	  sleep FLUSH_INTERVAL
+	  @mutex.synchronize do
+	    if @buffered
+	      @log_out.flush
+	      @buffered = false
+	    end
+	  end
+	end
+      end
     end
 
     def message(message)
-      @mutex.synchronize{@log_out.puts message}
+      @mutex.synchronize do
+	@log_out.puts message
+	@buffered = true
+      end
     end
   end
 end
