a7caf03180e4089a12288fe2b27619e98e5f5409
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-10-29 11:09:47 +0000

    * share/conf.rb, etc/fairy.conf: 追加  * fairy.rb: それに伴う修正  * master.rb, node.rb, cotroller.rb, processor.rb: それに伴う修正  * bin/{master, node, controller, processor}: それに伴う修正
    git-svn-id: http://localhost/svn/repos/fairy@310 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.6-022-conf/backend/b-zipper.rb
@@ -0,0 +1,60 @@
+
+require "backend/b-filter"
+require "backend/b-inputtable"
+
+module Fairy
+  class BZipper<BFilter
+    Controller.def_export self
+
+    ZIP_BY_SUBSTREAM = :ZIP_BY_SUBSTREAM
+
+    DeepConnect.def_single_method_spec(self, "REF new(REF, VAL, VAL, REF)")
+
+    def initialize(controller, opts, others, block_source)
+      super
+      @others = others
+      @block_source = block_source
+    end
+
+    def opt_zip_by_substream?
+      @opts[ZIP_BY_SUBSTREAM]
+    end
+
+    def node_class_name
+      "NZipper"
+    end
+
+    def njob_creation_params
+      [@block_source]
+    end
+
+    def start_create_nodes
+      @other_export_queues = @others.collect{|other|
+	exports = Queue.new
+	Thread.start do
+	  other.each_export do |export, node|
+	    exports.push export
+	  end
+	end
+	exports
+      }
+      super
+    end
+
+    def create_and_add_node(export, node)
+      node = super
+      if opt_zip_by_substream?
+	others = @other_export_queues.collect{|queue| queue.pop	}
+	node.zip_inputs = others
+	others.zip(node.zip_imports){|other, import| other.output = import}
+      else
+	raise "まだできていません"
+      end
+    end
+
+    def break_running
+      super
+      @others.each{|others| Thread.start{others.break_running}}
+    end
+  end
+end
