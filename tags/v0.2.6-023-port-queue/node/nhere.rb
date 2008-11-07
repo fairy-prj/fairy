a005b08a49986f3c7ab5b6d135df327f4f0f90b0
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-11-07 09:54:07 +0000

    * port.rb: Queueもサポート. SizedQueueだと, GroupByの後に, シーケン    シャルな処理が入るとデッドロックすることが分かったため.  * log.rb: requie stringio の追加
    git-svn-id: http://localhost/svn/repos/fairy@313 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.6-023-port-queue/node/nhere.rb
@@ -0,0 +1,23 @@
+
+require "node/n-filter"
+require "node/n-single-exportable"
+
+module Fairy
+  class NHere<NFilter
+    Processor.def_export self
+
+    include NSingleExportable
+
+    def start
+      super do
+	@import.each do |e|
+# 	  if e.__deep_connect_reference? && e.kind_of?(Array)
+# 	    e = e.to_a
+# 	  end
+#Log::debug(self, "%s: %s", self, e)
+	  @export.push e
+	end
+      end
+    end
+  end
+end
