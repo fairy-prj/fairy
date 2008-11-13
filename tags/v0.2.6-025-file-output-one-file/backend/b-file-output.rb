0d68dc0d1e80ab2d139510247d5d37acea75594b
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-11-13 10:16:30 +0000

    * b-file-output.rb: one_file_by_processサポート
    git-svn-id: http://localhost/svn/repos/fairy@322 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.6-025-file-output-one-file/backend/b-file-output.rb
@@ -0,0 +1,78 @@
+
+require "controller"
+require "backend/boutput"
+
+module Fairy
+  class BFileOutput<BOutput
+    Controller.def_export self
+
+    def initialize(controller, opts)
+      super
+      @vfile = nil
+
+      @one_file_by_procs = @opts[:one_file_by_process]
+    end
+
+    def output(vf)
+      @vfile = vf
+    end
+
+    def node_class_name
+      "NFileOutput"
+    end
+
+    def njob_creation_params
+      [@vfile]
+    end
+
+    def create_nodes
+      return create_node_one_file if @one_file_by_procs
+      no = 0
+      @input.each_export do |input_export, input_njob|
+	njob = create_and_add_node(input_export, input_njob)
+	njob.add_input(nil)
+	no += 1
+      end
+      self.number_of_nodes = no
+    end
+
+    def create_node_one_file
+      no = 0
+      input_processors = {}
+      @input.each_export do |input_export, input_njob|
+	if njob = input_processors[input_njob.processor]
+	  njob.add_input(input_export)
+	else
+	  njob = create_and_add_node(input_export, input_njob)
+	  input_processors[njob.processor] = njob
+	  no += 1
+	end
+      end
+      for p, njob in input_processors
+	njob.add_input(nil)
+      end
+      self.number_of_nodes = no
+    end
+
+
+    def wait_all_output_finished
+      while !all_node_outputted?
+	@nodes_status_mutex.synchronize do
+	  @nodes_status_cv.wait(@nodes_status_mutex)
+	end
+      end
+    end
+
+    def all_node_outputted?
+      return false unless @number_of_nodes
+
+      all_outputted = true
+      each_node(:exist_only) do |node|
+	st = @nodes_status[node]
+	all_imported &= [:ST_FINISH, :ST_OUTPUT_FINISH].include?(st)
+      end
+      all_outputted
+    end
+
+  end
+end
---
+++ tags/v0.2.6-025-file-output-one-file/node/n-single-exportable.rb
@@ -0,0 +1,54 @@
+
+require "node/n-filter"
+ 
+module Fairy
+  module NSingleExportable
+    END_OF_STREAM = NJob::END_OF_STREAM
+
+    ST_WAIT_EXPORT_FINISH = :ST_WAIT_EXPORT_FINISH
+    ST_EXPORT_FINISH = :ST_EXPORT_FINISH
+
+    def initialize(processor, bjob, opts=nil, *rests)
+      super
+      @export = Export.new unless @export
+    end
+
+    attr_reader :export
+
+    def no=(no)
+      super
+      @export.no = no
+    end
+
+    def start(&block)
+      super do
+	begin
+	  if @import
+	    @export.add_key(@import.key)
+	  end
+	  block.call
+	ensure
+#Log::debug(self, "EOS")
+	  @export.push END_OF_STREAM
+	  wait_export_finish
+	end
+      end
+    end
+
+    def wait_export_finish
+      self.status = ST_WAIT_EXPORT_FINISH
+      @export.wait_finish
+      self.status = ST_EXPORT_FINISH
+    end
+  end
+
+  class NSingleExportFilter<NFilter
+    include NSingleExportable
+  end
+
+
+  class NSingleExportInput<NFilter
+    include NSingleExportable
+  end
+
+end
