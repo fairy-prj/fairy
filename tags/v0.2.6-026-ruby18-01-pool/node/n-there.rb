235970b35c48dca06ec08f0ab9d9fff210fc542d
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-11-21 10:39:20 +0000

    * node/addins.rb, backend/addins.rb: ruby1.8対応(PoolVariable)
    git-svn-id: http://localhost/svn/repos/fairy@325 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.6-026-ruby18-01-pool/node/n-there.rb
@@ -0,0 +1,24 @@
+
+require "node/njob"
+require "node/port"
+require "node/n-single-exportable"
+
+module Fairy
+  class NThere<NJob
+    Processor.def_export self
+
+    include NSingleExportable
+
+
+    def initialize(processor, bjob, opts, enumerable)
+      super
+      @enumerable = enumerable
+    end
+
+    def start
+      super do
+	@enumerable.each{|e| @export.push e}
+      end
+    end
+  end
+end
