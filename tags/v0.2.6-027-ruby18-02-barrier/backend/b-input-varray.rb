ede7aa6ec8023c8b76a3f9b67ce3429416e8eb83
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-11-25 10:03:29 +0000

    * ruby1.8対応(barrier)    + backend/bjob.rb: number_of_nodes: ConditionVariable名間違い  * share/log.rb: ***_exceptionのaliasを間違っていた
    git-svn-id: http://localhost/svn/repos/fairy@328 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.6-027-ruby18-02-barrier/backend/b-input-varray.rb
@@ -0,0 +1,39 @@
+
+module Fairy
+
+  class BInputVArray<BInput
+    Controller.def_export self
+
+    def initialize(controller, opts, varray)
+      super
+      @varray = varray
+    end
+
+    def node_class_name
+      "NInputVArray"
+    end
+
+    def create_and_start_nodes
+      begin
+	no = 0
+	@varray.arrays_size.times do 
+	  @create_node_mutex.synchronize do
+	    subarray = @varray.arrays_at(no)
+	    @controller.assign_processor(self, 
+					 :SAME_PROCESSOR_OBJ, 
+					 subarray) do |processor|
+	      njob = create_node(processor, subarray)
+	      njob.start
+	      no +=1
+	    end
+	  end
+	end
+      rescue BreakCreateNode
+	# do nothing
+	Log::debug self, "BREAK CREATE NODE: #{self}" 
+      ensure
+	self.number_of_nodes = no
+      end
+    end
+  end
+end
---
+++ tags/v0.2.6-027-ruby18-02-barrier/job/local-file-output.rb
@@ -0,0 +1,44 @@
+
+require "job/job"
+require "share/vfile"
+
+module Fairy
+  class LFileOutput < Job
+
+    @backend_class = nil
+
+    def LFileOutput.output(fairy, opts, filename)
+      ffile = new(fairy, opts)
+      ffile.output(filename)
+      ffile
+    end
+
+    def initialize(fairy, opts=nil)
+      super
+      @opts = opts
+      
+      @filename = nil      
+    end
+
+    def backend_class_name
+      "BLFileOutput"
+    end
+
+    def output(filename)
+      @filename = filename
+      backend.output(self)
+
+    end
+
+    def input=(job)
+      @input = job
+      backend.input=job.backend
+      
+      File.open(@filename, "w") do |io|
+	for l in backend
+	  io.puts l
+	end
+      end
+    end
+  end
+end
