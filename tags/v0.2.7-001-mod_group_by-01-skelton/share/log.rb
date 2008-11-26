86c47eacf03dab55d341904baa9692099ef0ec89
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-11-26 09:33:54 +0000

    * mod_gorup_by   + ModGroupBy, BModGroupBy, NModGroupBy: 完成   + PreAfterModFilter, NPreAfterModFiler: 完成   + PostAfterModFilter, NPostAfterModFiler: 完成   + BPreAfterModFilter, BPostAtferModFilter: スケルトン   + NGroupBy#start, key: サブクラス対応   + conf.rb: N_MOD_GROUP_BY: 追加  * Fairy::post_initialize: 追加  * job/addind.rb: post_initialize対応
    git-svn-id: http://localhost/svn/repos/fairy@332 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.7-001-mod_group_by-01-skelton/share/log.rb
@@ -0,0 +1,128 @@
+
+require "thread"
+require "stringio"
+
+require "forwardable"
+
+require "deep-connect/future"
+
+module Fairy
+  class Log
+
+    LEVELS = [:FATAL, :ERROR, :WARN, :INFO, :DEBUG]
+    MESSAGE_LEVEL = CONF.LOG_LEVEL
+    
+    PRINT_STDOUT = true
+
+    def initialize
+      @logger = nil
+      @host = `hostname`.chomp
+      @type = $0
+      @pid = nil
+
+      @mutex = Mutex.new
+    end
+
+    @the_log = Log.new
+
+    class<<self
+      extend Forwardable
+
+      def method_added(method)
+	(class<<self;self;end).def_delegator :@the_log, method
+      end
+    end
+
+    attr_accessor :logger
+    attr_accessor :type
+    attr_accessor :pid
+
+    # Log::log(sender, format, args...)
+    # Log::log(format, args,...)
+    def log(sender, format=nil, *args, &block)
+      bt = caller(0).select{|l| /fairy.*(share\/log)|__FORWARDABLE__/ !~ l}
+      bt.first =~ /\/([^\/]*\.rb):([0-9]+):in `(.*)'$/
+      file_name = $1
+      line_no = $2
+      method = $3
+
+      if sender.kind_of?(String)
+	format = sender
+	sender_type = "[UNDEF]"
+      else
+	sender_type = sender.class.name.sub(/Fairy::/, "")
+      end
+
+      time = Time.now
+      prefix = time.strftime("%Y/%m/%d %H:%M:%S")
+      prefix.concat sprintf(".%06d %s ", time.usec, @host)
+      mes = sprintf("%s%s%s %s[%s] %s#%s: ", 
+		    @type, 
+		    @pid ? "\##{@pid}": "", 
+		    Thread.current["name"] ? Thread.current["name"]: "",
+		    file_name, line_no,
+		    sender_type, method)
+      if block_given?
+	sio = StringIO.new(mes, "a+")
+	yield sio
+      else
+	mes.concat sprintf(format, *args)
+      end
+      mes.chomp!
+      stdout_puts mes if PRINT_STDOUT
+
+      if @logger
+	DeepConnect.future{@mutex.synchronize{@logger.message(prefix+mes)}} 
+      else
+	stdout_puts "****Loggerが設定されていません****"
+      end
+    end
+    alias stdout_puts puts
+    alias puts log
+
+    # Log::log_exception(sender, exception, level = :WARN)
+    # Log::log_exception(exception, level = :WARN)
+    def log_exception(sender = $!, exception=$!)
+      if sender.kind_of?(Exception)
+	exception = sender
+	sender = "UNDEF"
+      end
+      log(sender) do |sio|
+	sio.puts exception
+	for l in exception.back_trace
+	  sio.puts l
+	end
+      end
+    end
+
+    def nop(*args); end
+
+    range = LEVELS[0..LEVELS.index(MESSAGE_LEVEL)]
+    if range
+      for level in range
+	method = level.id2name.downcase
+	alias_method method, :log
+	alias_method method+"_exception", :log_exception
+      end
+    end
+
+    range = LEVELS[LEVELS.index(MESSAGE_LEVEL)+1.. -1]
+    if range
+      for level in range
+	method = level.id2name.downcase
+	alias_method method, :nop
+	alias_method method+"_exception", :nop
+      end
+    end
+
+    if MESSAGE_LEVEL == :DEBUG
+      def debug_p(*objs)
+	for o in objs
+	  log(o.inspect)
+	end
+      end
+    else
+      def debug_p(*args);end      
+    end
+  end
+end
