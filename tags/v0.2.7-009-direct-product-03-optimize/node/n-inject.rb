e292aa714ebcbacc51209c9ef5df00bc7f3295d7
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-12-09 11:16:25 +0000

    * NDirectProduct::NPostFilter#start: 最適化した
    git-svn-id: http://localhost/svn/repos/fairy@354 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.7-009-direct-product-03-optimize/node/n-inject.rb
@@ -0,0 +1,72 @@
+
+require "node/njob"
+require "node/n-single-exportable"
+
+module Fairy
+
+  class NInject<NSingleExportFilter
+    def initialize(processor, bjob, opts, block_source)
+      super
+
+      @init_value = :__FAIRY_NO_VALUE__
+      if @opts.key?(:init_value)
+	@init_value = @opts[:init_value]
+      end
+      @block_source = block_source
+#      @inject_proc = @context.create_proc(@block_source)
+    end
+
+    def start
+      super do
+	@inject_proc = BBlock.new(@block_source, @context, self)
+	sum = @init_value
+	@import.each do |e|
+	  if sum == :__FAIRY_NO_VALUE__
+	    sum = e
+	  else
+	    sum = @inject_proc.yield(sum, e)
+	  end
+	end
+	finish(sum)
+      end
+    end
+  end
+
+  class NLocalInject<NInject
+    Processor.def_export self
+    
+    def finish(sum)
+      @export.push sum
+    end
+
+  end
+
+  class NWideInject<NInject
+    Processor.def_export self
+
+    def initialize(*args)
+      super
+
+      @value = :__FAIRY_NO_VALUE__
+      @value_mutex = Mutex.new
+      @value_cv = ConditionVariable.new
+    end
+
+    def value
+      @value_mutex.synchronize do
+	while @value == :__FAIRY_NO_VALUE__
+	  @value_cv.wait(@value_mutex)
+	end
+	@value
+      end
+    end
+
+    def finish(sum)
+      @value = sum
+      @value_cv.broadcast 
+      if @export
+	@export.push sum
+      end
+    end
+  end
+end
