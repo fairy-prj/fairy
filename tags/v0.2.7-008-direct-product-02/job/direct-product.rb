05e5111caf07899e427432f46ad14ebdd4e9110f
(no author) <(no author)@a864a6dc-dab1-42d4-9391-a69d331a00af>
2008-12-09 09:18:06 +0000

    * 追加忘れ
    git-svn-id: http://localhost/svn/repos/fairy@351 a864a6dc-dab1-42d4-9391-a69d331a00af

---
+++ tags/v0.2.7-008-direct-product-02/job/direct-product.rb
@@ -0,0 +1,47 @@
+
+require "job/filter"
+
+module Fairy
+  class DirectProduct<Filter
+
+    module Interface
+      # jpb.direct_product(opts,...,filter,...,block_source, opts,...)
+      def direct_product(*others)
+	block_source = nil
+	if others.last.kind_of?(String)
+	  block_source = others.pop
+	end
+	others, opts = others.partition{|e| e.kind_of?(Job)}
+	if opts.last.kind_of?(Hash)
+	  h = opts.pop
+	else
+	  h = {}
+	end
+	opts.each{|e| h[e] = true}
+
+	block_source = BlockSource.new(block_source) 
+	dp = DirectProduct.new(@fairy, h, others, block_source)
+	dp.input = self
+	dp
+      end
+      alias product direct_product
+
+      def *(other)
+	direct_product(other, %{|e| e})
+      end
+    end
+    Fairy::def_job_interface Interface
+
+    def initialize(fairy, opts, others, block_source)
+      super(fairy, opts, others.collect{|o| o.backend}, block_source)
+      @others = others
+      @block_source
+      @opts = opts
+    end
+
+    def backend_class_name
+      "BDirectProduct"
+    end
+  end
+end
+
