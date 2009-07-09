2ea984525903da9370dd7e22edadaa86475b20e8
Keiju Ishitsuka <keiju@ishitsuka.com>
2009-07-09 16:48:23 +0900

    * block-source.rb: BBlock.yeldをちょっと最適化  * block-source.rb, conf.rb: CONF.BLOCK_USE_STDOUT 対応

--- lib/fairy/share/block-source.rb
+++ lib/fairy/share/block-source.rb
@@ -75,22 +75,11 @@ module Fairy
       end
     end
 
-    def yield(*args)
+    def yield19(*args)
       begin
-	if @block.respond_to?(:yield)
-	  $stdout.replace_stdout do
-	    @block.yield(*args)
-	  end
-	else
-	  if args.size == 1 && args.first.kind_of?(Array)
-	    args = args.first.to_a
-	  end
-	  $stdout.replace_stdout do
-	    @block.call(*args)
-	  end
+	$stdout.replace_stdout do
+	  @block.yield(*args)
 	end
-#       rescue @context.class::LocalBreak
-# 	Log::debug(self, "CAUGHT LocalBreak")
 
       rescue LocalJumpError, @context.class::GlobalBreak
 	Log::debug(self, "CAUGHT GlobalBreak")
@@ -112,6 +101,72 @@ module Fairy
 
 	@exception_handler.handle_exception($!)
       end
+    end
+
+    def yield19_no_use_stdout(*args)
+      begin
+	@block.yield(*args)
+      rescue LocalJumpError, @context.class::GlobalBreak
+	Log::debug(self, "CAUGHT GlobalBreak")
+	@exception_handler.global_break
+
+      rescue Exception
+	Log::warn(self) do |sio|
+	  sio.puts "Warn: Exception raised:"
+	  sio.puts $!
+	  for l in $@
+	    sio.puts "\t#{l}"
+	  end
+	end
+	bt = $!.backtrace
+	bt = bt.select{|l| /fairy.*(share|job|backend|node|processor|controller)|deep-connect|__FORWARDABLE__|bin.*processor/ !~ l} unless CONF.DEBUG_FULL_BACKTRACE
+	bt.first.sub!("bind", @block_source.caller_method)
+	bt.push *@block_source.backtrace.dc_deep_copy
+	$!.set_backtrace(bt)
+
+	@exception_handler.handle_exception($!)
+      end
+    end
+
+    def yield18(*args)
+      begin
+	if args.size == 1 && args.first.kind_of?(Array)
+	  args = args.first.to_a
+	end
+	$stdout.replace_stdout do
+	  @block.call(*args)
+	end
+
+      rescue LocalJumpError, @context.class::GlobalBreak
+	Log::debug(self, "CAUGHT GlobalBreak")
+	@exception_handler.global_break
+
+      rescue Exception
+	Log::warn(self) do |sio|
+	  sio.puts "Warn: Exception raised:"
+	  sio.puts $!
+	  for l in $@
+	    sio.puts "\t#{l}"
+	  end
+	end
+	bt = $!.backtrace
+	bt = bt.select{|l| /fairy.*(share|job|backend|node|processor|controller)|deep-connect|__FORWARDABLE__|bin.*processor/ !~ l} unless CONF.DEBUG_FULL_BACKTRACE
+	bt.first.sub!("bind", @block_source.caller_method)
+	bt.push *@block_source.backtrace.dc_deep_copy
+	$!.set_backtrace(bt)
+
+	@exception_handler.handle_exception($!)
+      end
+    end
+
+    if proc{}.respond_to?(:yield)
+      if CONF.BLOCK_USE_STDOUT
+	alias yield yield19
+      else
+	alias yield yield19_no_use_stdout
+      end
+    else
+      alias yield yield18
     end
     alias call yield
   end
