86a4364f9cc6dcdbedaaa206a9215a2bbdbeec2d
Keiju Ishitsuka <keiju@ishitsuka.com>
2010-03-09 18:04:08 +0900

    * processor.rb, port.rb: [REQ#218]対応

--- test/testc.rb
+++ test/testc.rb
@@ -3544,7 +3544,7 @@ when "69.2.4"
 #  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_10M.txt"]*10)
 #  f = fairy.input(["file://giant/home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/fairy.cat"]*1)
 #  f = fairy.input(["file://emperor/home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/fairy.cat"]*1)
-  f = fairy.input(["file://giant//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_10M.txt"]*1)
+  f = fairy.input(["file://giant//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_10M.txt"]*10)
   f = f.mapf(%{|ln| begin
                       ln.chomp.split
 		    rescue
