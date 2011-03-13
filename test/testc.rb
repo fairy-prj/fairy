7d532a40ad66c1fd39c53aee9818f285c4fddd91
Keiju Ishitsuka <keiju@ishitsuka.com>
2011-03-13 23:41:38 +0900

    * testc.rb: ちょっと修正

--- test/testc.rb
+++ test/testc.rb
@@ -7108,7 +7108,8 @@ when "113.0"
   f.here.each{|e| puts e}
 
 when "114"
-  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_120M.txt"])
+  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_960M.txt"])
+#  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_120M.txt"])
 #  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/fairy.cat"]*1)
   f = f.mapf(%{|ln| begin
                       ln.chomp.split
