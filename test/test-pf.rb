58190c0d9a47eae6091c8e2e8dbf3969f7e18c74
Keiju Ishitsuka <keiju@ishitsuka.com>
2011-04-05 01:14:18 +0900

    * test-pf.rb: ちょっと修正

--- test/test-pf.rb
+++ test/test-pf.rb
@@ -307,4 +307,32 @@ when "13"
   #  f.here.each{|e| puts e.join(" ")}
 
 
+when "14"
+
+  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_240M.txt"]*1)
+#  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/sample_10M.txt"]*1)
+#  f = fairy.input(["file://emperor//home/keiju/public/a.research/fairy/git/fairy/sample/wc/data/fairy.cat"]*1)
+  f = f.mapf(%{|ln| begin
+                      ln.chomp.split
+		    rescue
+		      []
+		    end
+  })
+  f = f.xgroup_by(%{|w| w},
+		  :no_segment => 1,
+		  :postqueuing_policy => {
+		    :queuing_class => :XMarshaledQueue,
+		    :chunk_size => 10000,},
+		  :buffering_policy => {
+		    :buffering_class => :XDirectMergeSortBuffer,
+		    :threshold => 400_000,
+		    :CHUNCK_SIZE => 10000},
+		  :postfilter_prequeuing_policy => {
+		    :queuing_class => :XMarshaledQueue,
+		    :chunk_size => 10000,},)
+  f = f.map(%{|values| [values.key, values.size].join(" ")})
+  f.output("test/test-pf.vf")
+  #  f.here.each{|e| puts e.join(" ")}
+
+
 end
